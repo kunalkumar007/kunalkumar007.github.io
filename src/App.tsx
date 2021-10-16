bacffc2d7d2ee797ef05fa10e0f4c4c486c19341
kunal kumar <kunalkumar06022003@gmail.com>
2021-10-16 15:38:16 +0530

    feat: added Readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import About from "./components/About";
 import { HeaderMenu, HomeHeader } from "./components/Header";
 import "./styles/app.css";
